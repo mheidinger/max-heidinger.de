22157df217a06ed1dbde248c181273d7e417d2d5
Max Heidinger <mail@max-heidinger.de>
2018-06-23 14:27:33 +0200

    Add some transitions

--- js/main.js
+++ js/main.js
@@ -1,36 +1,49 @@
-$(document).ready(function() {
-	$("#start_container #learn_more_button").click(function() {
-		doTransition("#start_container", "#full_site_container");
+(function() {
+	$(document).ready(function() {
+		$("#start_container #learn_more_button").click(function() {
+			pageTransition("#start_container", "#full_site_container");
+			$("#start_container_content").removeClass("first_open");
+		});
+
+		$("#full_site_container #header").click(function() {
+			pageTransition("#full_site_container", "#start_container");
+		});
+
+		$("#data_privacy_link").click(function() {
+			modalTransition(true);
+		});
+
+		$(window).click(function(ev) {
+			if (ev.target.className == "modal") {
+				modalTransition(false);
+			}
+		});
+
+		$(document).keyup(function(ev) {
+			if (ev.keyCode === 27) {
+				modalTransition(false);
+			}
+		});
 	});
 
-	$("#full_site_container #header").click(function() {
-		doTransition("#full_site_container", "#start_container");
-	});
+	function pageTransition(from, to) {
+		transition.begin($(from)[0], "transform scale(1) scale(0) 0.2s ease-in-out", {
+			onTransitionEnd: () => {
+				$(from).css("display", "none");
+				$(to).css("display", "flex");
+				transition.begin($(to)[0], "transform scale(0) scale(1) 0.2s ease-in-out");
+			}
+		});
+	};
 
-	let modal = $(".modal");
-	$("#data_privacy_link").click(function() {
-		modal.css("display", "block");
-	});
-
-	$(window).click(function(ev) {
-		if (ev.target.className == "modal") {
-			modal.css("display", "none");
+	function modalTransition(open) {
+		let modal = $(".modal");
+		let modalContent = $(".modal_content");
+		if (open) {
+			modal.css("display", "block");
+			transition.begin(modalContent[0], "transform scale(0) scale(1) 0.1 ease-in-out")
+		} else {
+			transition.begin(modalContent[0], "transform scale(1) scale(0) 0.1 ease-in-out", { onTransitionEnd: () => modal.css("display", "none") });
 		}
-	});
-
-	$(document).keyup(function(ev) {
-		if (ev.keyCode === 27) {
-			modal.css("display", "none");
-		}
-	});
-});
-
-function doTransition(from, to) {
-	transition.begin($(from)[0], "transform scale(1) scale(0) 0.2s ease-in-out", {
-		onTransitionEnd: function() {
-			$(from).css("display", "none");
-			$(to).css("display", "flex");
-			transition.begin($(to)[0], "transform scale(0) scale(1) 0.2s ease-in-out");
-		}
-	});
-};
+	}
+})();
